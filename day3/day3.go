edeb8a45969d0f1bdfc94f1ead0eb9ba0961d787
Jacob Gulan <jacobpgulan@gmail.com>
2023-12-14 17:08:21 -0500

    finish day 3

--- day3/day3.go
+++ day3/day3.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 	"unicode"
 )
 
@@ -115,6 +116,254 @@ func GetNumbersNextToSymbol(puzzleInput [][]rune) map[string]int {
 	return numbers
 }
 
+func GetNumbersNextToSymbolAndAsterisks(puzzleInput [][]rune) map[string]int {
+	// Create slice to hold numbers
+	var numbersAndAsterisks map[string]int = make(map[string]int)
+
+	// Loop through each line and determine if there is a number adjacent to a symbol
+	// The number can be on the left, right, top, bottom, or diagonal
+	// The number continues until it hits a period or another symbol
+	for i, runeRow := range puzzleInput {
+		for j, r := range runeRow {
+			if isSymbol(r) {
+				// Store the presence of an asterisk with the key being the row and column
+				if r == '*' {
+					numbersAndAsterisks[fmt.Sprintf("%d:%d", i, j)] = '*'
+				}
+
+				// Check left
+				if j-1 >= 0 {
+					if unicode.IsDigit(runeRow[j-1]) {
+						startIndex := getStartIndex(runeRow, j-1)
+						value, position := getNumberAndPosition(runeRow, startIndex, i)
+						if value != -1 {
+							numbersAndAsterisks[position] = value
+						}
+					}
+				}
+
+				// Check right
+				if j+1 < len(runeRow) {
+					if unicode.IsDigit(runeRow[j+1]) {
+						startIndex := getStartIndex(runeRow, j+1)
+						value, position := getNumberAndPosition(runeRow, startIndex, i)
+						if value != -1 {
+							numbersAndAsterisks[position] = value
+						}
+					}
+				}
+
+				// Check top
+				if i-1 >= 0 {
+					if unicode.IsDigit(puzzleInput[i-1][j]) {
+						startIndex := getStartIndex(puzzleInput[i-1], j)
+						value, position := getNumberAndPosition(puzzleInput[i-1], startIndex, i-1)
+						if value != -1 {
+							numbersAndAsterisks[position] = value
+						}
+					}
+				}
+
+				// Check bottom
+				if i+1 < len(puzzleInput) {
+					if unicode.IsDigit(puzzleInput[i+1][j]) {
+						startIndex := getStartIndex(puzzleInput[i+1], j)
+						value, position := getNumberAndPosition(puzzleInput[i+1], startIndex, i+1)
+						if value != -1 {
+							numbersAndAsterisks[position] = value
+						}
+					}
+				}
+
+				// Check top left
+				if i-1 >= 0 && j-1 >= 0 {
+					if unicode.IsDigit(puzzleInput[i-1][j-1]) {
+						startIndex := getStartIndex(puzzleInput[i-1], j-1)
+						value, position := getNumberAndPosition(puzzleInput[i-1], startIndex, i-1)
+						if value != -1 {
+							numbersAndAsterisks[position] = value
+						}
+					}
+				}
+
+				// Check top right
+				if i-1 >= 0 && j+1 < len(runeRow) {
+					if unicode.IsDigit(puzzleInput[i-1][j+1]) {
+						startIndex := getStartIndex(puzzleInput[i-1], j+1)
+						value, position := getNumberAndPosition(puzzleInput[i-1], startIndex, i-1)
+						if value != -1 {
+							numbersAndAsterisks[position] = value
+						}
+					}
+				}
+
+				// Check bottom left
+				if i+1 < len(puzzleInput) && j-1 >= 0 {
+					if unicode.IsDigit(puzzleInput[i+1][j-1]) {
+						startIndex := getStartIndex(puzzleInput[i+1], j-1)
+						value, position := getNumberAndPosition(puzzleInput[i+1], startIndex, i+1)
+						if value != -1 {
+							numbersAndAsterisks[position] = value
+						}
+					}
+				}
+
+				// Check bottom right
+				if i+1 < len(puzzleInput) && j+1 < len(runeRow) {
+					if unicode.IsDigit(puzzleInput[i+1][j+1]) {
+						startIndex := getStartIndex(puzzleInput[i+1], j+1)
+						value, position := getNumberAndPosition(puzzleInput[i+1], startIndex, i+1)
+						if value != -1 {
+							numbersAndAsterisks[position] = value
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return numbersAndAsterisks
+}
+
+func SumPart2Numbers(inputMatrix [][]rune, numbersAndAsterisks map[string]int) int {
+	part2TotalSum := 0
+
+	// Loop through each number and multiply adjacent numbers by each other if an asterisk is found as their adjacent symbol
+	// Delete the asterisk and the number from the map after multiplying
+	// The number can be on the left, right, top, bottom, or diagonal
+	// The number is up to 3 digits long
+	for key, value := range numbersAndAsterisks {
+		if value != '*' {
+			continue
+		}
+
+		// Get row and column
+		rowAndColumn := key
+		rowAndColumnSplit := strings.Split(rowAndColumn, ":")
+		row, err := strconv.Atoi(rowAndColumnSplit[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		column, err := strconv.Atoi(rowAndColumnSplit[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Make a map to hold the multiplying numbers and their keys
+		multipylingNumbers := make(map[string]int)
+
+		// Find the starting indicies of numbers adjacent to the asterisk
+		// Check left
+		if column-1 >= 0 {
+			if unicode.IsDigit(inputMatrix[row][column-1]) {
+				startIndex := getStartIndex(inputMatrix[row], column-1)
+				value, position := getNumberAndPosition(inputMatrix[row], startIndex, row)
+				if value != -1 {
+					multipylingNumbers[position] = value
+				}
+			}
+		}
+
+		// Check right
+		if column+1 < len(inputMatrix[row]) {
+			if unicode.IsDigit(inputMatrix[row][column+1]) {
+				startIndex := getStartIndex(inputMatrix[row], column+1)
+				value, position := getNumberAndPosition(inputMatrix[row], startIndex, row)
+				if value != -1 {
+					multipylingNumbers[position] = value
+				}
+			}
+		}
+
+		// Check top
+		if row-1 >= 0 {
+			if unicode.IsDigit(inputMatrix[row-1][column]) {
+				startIndex := getStartIndex(inputMatrix[row-1], column)
+				value, position := getNumberAndPosition(inputMatrix[row-1], startIndex, row-1)
+				if value != -1 {
+					multipylingNumbers[position] = value
+				}
+			}
+		}
+
+		// Check bottom
+		if row+1 < len(inputMatrix) {
+			if unicode.IsDigit(inputMatrix[row+1][column]) {
+				startIndex := getStartIndex(inputMatrix[row+1], column)
+				value, position := getNumberAndPosition(inputMatrix[row+1], startIndex, row+1)
+				if value != -1 {
+					multipylingNumbers[position] = value
+				}
+			}
+		}
+
+		// Check top left
+		if row-1 >= 0 && column-1 >= 0 {
+			if unicode.IsDigit(inputMatrix[row-1][column-1]) {
+				startIndex := getStartIndex(inputMatrix[row-1], column-1)
+				value, position := getNumberAndPosition(inputMatrix[row-1], startIndex, row-1)
+				if value != -1 {
+					multipylingNumbers[position] = value
+				}
+			}
+		}
+
+		// Check top right
+		if row-1 >= 0 && column+1 < len(inputMatrix[row]) {
+			if unicode.IsDigit(inputMatrix[row-1][column+1]) {
+				startIndex := getStartIndex(inputMatrix[row-1], column+1)
+				value, position := getNumberAndPosition(inputMatrix[row-1], startIndex, row-1)
+				if value != -1 {
+					multipylingNumbers[position] = value
+				}
+			}
+		}
+
+		// Check bottom left
+		if row+1 < len(inputMatrix) && column-1 >= 0 {
+			if unicode.IsDigit(inputMatrix[row+1][column-1]) {
+				startIndex := getStartIndex(inputMatrix[row+1], column-1)
+				value, position := getNumberAndPosition(inputMatrix[row+1], startIndex, row+1)
+				if value != -1 {
+					multipylingNumbers[position] = value
+				}
+			}
+		}
+
+		// Check bottom right
+		if row+1 < len(inputMatrix) && column+1 < len(inputMatrix[row]) {
+			if unicode.IsDigit(inputMatrix[row+1][column+1]) {
+				startIndex := getStartIndex(inputMatrix[row+1], column+1)
+				value, position := getNumberAndPosition(inputMatrix[row+1], startIndex, row+1)
+				if value != -1 {
+					multipylingNumbers[position] = value
+				}
+			}
+		}
+
+		if len(multipylingNumbers) == 0 || len(multipylingNumbers) == 1 {
+			delete(numbersAndAsterisks, key)
+			continue
+		}
+
+		// Multiply the numbers together
+		product := 1
+		for _, value := range multipylingNumbers {
+			product *= value
+		}
+
+		// Delete the asterisk and the number from the map
+		delete(numbersAndAsterisks, key)
+		for multiplyKey := range multipylingNumbers {
+			delete(numbersAndAsterisks, multiplyKey)
+		}
+		part2TotalSum += product
+
+	}
+
+	return part2TotalSum
+}
+
 func isSymbol(r rune) bool {
 	for _, symbol := range symbols {
 		if r == symbol {
@@ -123,6 +372,10 @@ func isSymbol(r rune) bool {
 	}
 	return false
 }
+
+// "3:144": 42
+// "2:144": 356
+// "3:111": 598
 
 func getInputMatrix() [][]rune {
 	// Open input.txt file
@@ -160,12 +413,10 @@ func getStartIndex(runeRow []rune, givenIndex int) int {
 // Given a rune row and a start index, return the number and its matrix position
 func getNumberAndPosition(runeRow []rune, startIndex int, rowNumber int) (int, string) {
 	number := ""
-	numberIndicies := ""
 
 	for i := startIndex; i < len(runeRow); i++ {
 		if unicode.IsDigit(runeRow[i]) {
 			number += string(runeRow[i])
-			numberIndicies += fmt.Sprintf("%d,", i)
 		} else {
 			break
 		}
@@ -179,7 +430,7 @@ func getNumberAndPosition(runeRow []rune, startIndex int, rowNumber int) (int, s
 		log.Fatal(err)
 	}
 
-	return value, fmt.Sprintf("%d:%s", rowNumber, numberIndicies)
+	return value, fmt.Sprintf("%d:%d", rowNumber, startIndex)
 }
 
 func main() {
@@ -198,4 +449,8 @@ func main() {
 
 	fmt.Println("Part 1:", totalSum)
 
+	var part2TotalSum int = 0
+	numbersAndAsterisks := GetNumbersNextToSymbolAndAsterisks(inputMatrix)
+	part2TotalSum = SumPart2Numbers(inputMatrix, numbersAndAsterisks)
+	fmt.Println("Part 2:", part2TotalSum)
 }
--- day3/day3_test.go
+++ day3/day3_test.go
@@ -4,22 +4,36 @@ import (
 	"testing"
 )
 
-func TestGetNumbersNextToSymbol(t *testing.T) {
-	inputString := [][]rune{
-		[]rune("...................548............456#..815....$...@.....357.........619......*.............@.................-.......................15...."),
-		[]rune(".865../....404..994.354........=.#738....535....+..=..........134.........................=..862.......215.939.......821*....#.............."),
-		[]rune("......*......&....284................887.183....#....446*220.749...........732....538...................327.......356...793.....703........."),
-		[]rune("...*................130...792........353.....*.795..........................383..690..53.....*.......385.&677..598*.......=....500.34./....."),
-	}
+var inputString = [][]rune{
+	[]rune("...................548............456#..815....$...@.....357.........619......*.............@.................-.......................15...."),
+	[]rune(".865../....404..994.354........=.#738....535....+..=..........134.........................=..862.......215.939.......821*....#.............."),
+	[]rune("......*......&....284................887.183....#....446*220.749...........732....538...................327.......356...793.....703........."),
+	[]rune("...*................130...792........353.....*.795..........................383..690..53.....*.......385.&677..598*.......=....500.34./....."),
+}
 
-	expected := [][]int{
+var inputString2 = [][]rune{
+	[]rune("467..114.."),
+	[]rune("...*......"),
+	[]rune("..35..633."),
+	[]rune("......#..."),
+	[]rune("617*......"),
+	[]rune(".....+.58."),
+	[]rune("..592....."),
+	[]rune("......755."),
+	[]rune("...$.*...."),
+	[]rune(".664.598.."),
+}
+
+func TestGetNumbersNextToSymbol(t *testing.T) {
+
+	expectedNumbers := [][]int{
 		{456},
 		{404, 738, 862, 939, 821},
 		{446, 220, 327, 356, 793},
 		{795, 677, 598},
 	}
 	sumExpected := 0
-	for _, row := range expected {
+	for _, row := range expectedNumbers {
 		for _, value := range row {
 			sumExpected += value
 		}
@@ -34,5 +48,22 @@ func TestGetNumbersNextToSymbol(t *testing.T) {
 	// Assert expected sum equals actual sum
 	if sumExpected != sumActual {
 		t.Errorf("Expected sum of %d, got %d", sumExpected, sumActual)
+	}
+}
+
+func TestSumPart2Numbers(t *testing.T) {
+	numbersAndAsterisks := GetNumbersNextToSymbolAndAsterisks(inputString)
+	expectedSum := (821 * 793) + (446 * 220) + (356 * 598)
+	actualSum := SumPart2Numbers(inputString, numbersAndAsterisks)
+
+	if expectedSum != actualSum {
+		t.Errorf("Part 2: Expected sum of %d, got %d", expectedSum, actualSum)
+	}
+
+	expectedSum2 := 467835
+	actualSum2 := SumPart2Numbers(inputString2, GetNumbersNextToSymbolAndAsterisks(inputString2))
+
+	if expectedSum2 != actualSum2 {
+		t.Errorf("Part 2: Expected sum of %d, got %d", expectedSum2, actualSum2)
 	}
 }
